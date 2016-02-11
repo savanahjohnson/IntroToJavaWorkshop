f653007eb05f0087fe069d3ce6fc273a260dd5ea
Spencer094 <league@Mac-mini.local>
2016-02-10 17:32:14 -0800

    savBot

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,11 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
-	
-		
-		
+Robot savBot = new Robot ();		
+for (int i = 0; i < 4; i++) {	
+savBot.penDown();
+savBot.move(200);
+savBot.turn(90);
+}	
 	}
 }
--- src/day1/robot/Spiral.java
+++ src/day1/robot/Spiral.java
@@ -5,23 +5,25 @@ import org.jointheleague.graphical.robot.Robot;
 public class Spiral {
 
 	public static void main(String[] args) {
-		// 1. Create a new Robot
 		
+		Robot savBot= new Robot();
 		// 5. Set your robot's pen to the down position
-
-		// 3. Set the robot to go at max speed (10)
-
-		// 4. Do the following (steps 7-9) 75 times
+            savBot.penDown();
+		
+           savBot.setSpeed(10);
+		
+for (int i = 0; i < 75; i++) {
+	
 
 			// 7. Change the pen color to random
-	
-			// 6. Move the robot 5 times the current line number you are drawing (5*i)
-	
-			// 2. Turn the robot 1/3 of 360 degrees to the right
-	
+	savBot.setRandomPenColor();
+			
+	savBot.move(5*i);
+			
+	savBot.turn(360/6);
 			// 8. Change the number of sides to 7 (don’t add a new line of code for this one!)
 	
 			// 9. Set the pen width to i
-
+}
 	}
 }
